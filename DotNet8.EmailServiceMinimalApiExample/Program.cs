dbb70b42238f4cae2da5d6eba61b38c524e6899e
minthukha <minthukha1239788@gmail.com>
2024-10-06 00:13:54 +0630

    Add project files.

---
+++ DotNet8.EmailServiceMinimalApiExample/Program.cs
@@ -0,0 +1,36 @@
+using DotNet8.EmailServiceMinimalApiExample.Models;
+using System.Net.Mail;
+using System.Net;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var smtpSetting = builder.Configuration.GetSection("SmtpSettings").Get<SmtpSettings>();
+
+//builder.Services.AddFluentEmail(smtpSetting!.User)
+builder.Services
+           .AddFluentEmail(smtpSetting!.User)
+           .AddRazorRenderer()
+           .AddSmtpSender(new SmtpClient(smtpSetting.Host)
+           {
+               Port = smtpSetting.Port,
+               Credentials = new NetworkCredential(smtpSetting.User, smtpSetting.Pass),
+               EnableSsl = smtpSetting.EnableSsl
+           });
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
