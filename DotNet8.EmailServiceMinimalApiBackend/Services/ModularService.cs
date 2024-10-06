478b3f004da55885953ddddbbb337034ba9272a1
minthukha <minthukha1239788@gmail.com>
2024-10-06 12:57:41 +0630

    feat : modular service

---
+++ DotNet8.EmailServiceMinimalApiBackend/Services/ModularService.cs
@@ -0,0 +1,25 @@
+﻿using DotNet8.EmailServiceMinimalApiBackend.Modules.Features.EmailService;
+
+namespace DotNet8.EmailServiceMinimalApiBackend.Services;
+
+public static class ModularService
+{
+    public static WebApplicationBuilder AddModularService(this WebApplicationBuilder builder)
+    {
+        return builder
+            .AddDataAcccessLayer()
+            .AddBusinessLogicLayer();
+    }
+
+    private static WebApplicationBuilder AddBusinessLogicLayer(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddScoped<EmailService>();
+        return builder;
+    }
+
+    private static WebApplicationBuilder AddDataAcccessLayer(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddScoped<EmailServiceRepository>();
+        return builder;
+    }
+}
