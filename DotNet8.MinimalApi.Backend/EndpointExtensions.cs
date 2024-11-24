296c7ed9847009c9466e747ad6b1a717b2ca4ff0
minthukha <minthukha1239788@gmail.com>
2024-11-24 22:16:43 +0630

    -

---
+++ DotNet8.MinimalApi.Backend/EndpointExtensions.cs
@@ -0,0 +1,32 @@
+﻿using DotNet8.MinimalApiProjectStructureExampleBackend.Endpoints;
+using System.Reflection;
+
+public static class EndpointExtensions
+{
+    public static IServiceCollection AddEndpoints(this IServiceCollection services, Assembly assembly)
+    {
+        var endpointTypes = assembly.GetTypes()
+            .Where(t => t.IsClass && !t.IsAbstract && typeof(IEndpoint).IsAssignableFrom(t));
+
+        foreach (var type in endpointTypes)
+        {
+            services.AddScoped(typeof(IEndpoint), type);
+        }
+
+        return services;
+    }
+
+    public static WebApplication MapEndpoints(this WebApplication app)
+    {
+        using (var scope = app.Services.CreateScope())
+        {
+            var scopedServices = scope.ServiceProvider;
+            var endpoints = scopedServices.GetServices<IEndpoint>();
+            foreach (var endpoint in endpoints)
+            {
+                endpoint.MapEndpoint(app);
+            }
+        }
+        return app;
+    }
+}
--- DotNet8.MinimalApi.Backend/Endpoints/EmailService/EmailServiceEndPoints.cs
+++ DotNet8.MinimalApi.Backend/Endpoints/EmailService/EmailServiceEndPoints.cs
@@ -1,15 +1,18 @@
-﻿namespace DotNet8.MinimalApiProjectStructureExampleBackend.Endpoints.EmailService;
+﻿using DotNet8.EmailServiceMinimalApiExample.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet8.MinimalApiProjectStructureExampleBackend.Endpoints.EmailService;
 
 public class EmailServiceEndPoints : IEndpoint
 {
     public void MapEndpoint(IEndpointRouteBuilder app)
     {
-        // app.MapPost("/api/class/list",
-        //     async (EmailRequestModel reqModel,
-        //     [FromServices] EmailService _service) =>
-        //     {
-        //         return await ClassList(reqModel, _service);
-        //     });
+        app.MapPost("/api/class/list",
+            async (EmailRequestModel reqModel,
+            [FromServices] EmailService _service) =>
+            {
+                return await ClassList(reqModel, _service);
+            });
     }
 
     // public async Task<string> Send(EmailRequestModel reqModel, EmailService _service)
