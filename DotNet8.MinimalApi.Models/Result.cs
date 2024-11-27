ca7a121ac35700bfa4d043a403347c635725fc0e
minthukha <minthukha1239788@gmail.com>
2024-11-27 22:35:16 +0630

    feat: result pattern

---
+++ DotNet8.MinimalApi.Models/Result.cs
@@ -0,0 +1,25 @@
+﻿namespace DotNet8.EmailServiceMinimalApi.Models;
+
+public class Result<T>
+{
+    public bool IsSuccess { get; set; }
+    public bool IsError => !IsSuccess;
+    public string? Message { get; set; }
+    public T? Data { get; set; }
+
+    public static Result<T> SuccessResult(T data, string message = "Success")
+    {
+        return new Result<T> { IsSuccess = true, Data = data, Message = message };
+    }
+
+    public static Result<T> SuccessResult(string message = "Success")
+    {
+        return new Result<T> { IsSuccess = true, Message = message };
+    } 
+    
+    public static Result<T> FailureResult(string message = "Fail")
+    {
+        return new Result<T> { IsSuccess = false, Message = message };
+    }
+    
+}
